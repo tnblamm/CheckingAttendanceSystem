1a466e20493c71642b691c7ed420106e7c7380e6
tnblamm <1459028@itec.hcmus.edu.vn>
2017-11-22 22:00:13 +0700

    Initial Commit

---
+++ CheckingAttendanceSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckingAttendanceSystem
+//
+//  Created by Lâm Trần Nguyễn Bảo on 11/22/17.
+//  Copyright © 2017 Lâm Trần Nguyễn Bảo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
